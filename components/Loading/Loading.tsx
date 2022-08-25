73cb19378c33620d38e06119b8fc7ecc95f03747
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-08-25 11:07:36 -0700

    file reorg

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+
+import { ThemeContext } from "../../contexts/Theme";
+import * as S from "./styles";
+export const Loading: React.FC = () => {
+    const { lightMode } = useContext(ThemeContext);
+    return <S.Loader lightMode={lightMode} data-testid="loading" />;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../contexts/AuthContext';
+import { ThemeContextProvider } from '../contexts/Theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+function useState(arg0: boolean): [any, any] {
+  throw new Error('Function not implemented.');
+}
