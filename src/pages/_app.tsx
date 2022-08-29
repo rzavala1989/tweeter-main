81b663cbc2e4a8b23da78548bc60695e7263b6e8
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-08-29 11:44:07 -0700

    Cleanup complete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../contexts/AuthContext';
 import { ThemeContextProvider } from '../contexts/Theme';
